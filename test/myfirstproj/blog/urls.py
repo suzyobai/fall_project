364b12475894fa4475135deb7d5d7d4696f808e0
unknown <spandanaa10@gmail.com>
2024-10-15 14:26:10 -0400

    making modifications to add page

--- test/myfirstproj/blog/urls.py
+++ test/myfirstproj/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import home,add
 urlpatterns = [
-	path("", home)
+	path("", home),
+    path("add", add)
 ]
--- test/myfirstproj/blog/views.py
+++ test/myfirstproj/blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 def home(request): 
     return render(request, "main.html")
+def add(request):
+    return redirect("/blog")
