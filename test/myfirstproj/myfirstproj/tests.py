8f7b5fbadf5395030229b50d6dcfba385b32b8cd
Kelly Jasmine Jacinto Ozuna <kellyjacintoozuna@gmail.com>
2024-10-17 10:25:15 -0400

    message

---
+++ test/myfirstproj/myfirstproj/tests.py
@@ -0,0 +1,15 @@
+'''
+Notes for me (;
+git add .
+git commit -m 
+git push origin main
+'''
+# Create your tests here.
+
+#edited by kelly not offically pushed yet
+
+# tests.py
+from django.test import TestCase
+from .models import Content, Genre, Language, Review
+from django.contrib.auth.models import User
+
