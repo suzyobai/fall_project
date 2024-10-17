f63573685baceba526dbcde109467d55d2a8b6cb
unknown <spandanaa10@gmail.com>
2024-10-17 10:43:00 -0400

    added User, SignIn, SignUp data model test cases

--- test/myfirstproj/myfirstproj/models.py
+++ test/myfirstproj/myfirstproj/models.py
@@ -1,5 +1,5 @@
 # I (kelly) created to be imported into test cases to test //not official
-
+# Spandana Andhavarapu: added User,SignIn, and SignUp models 
 ##Definitions for data models
 
 from django.db import models
@@ -55,4 +55,35 @@ class UserProfile(models.Model):
    def __str__(self):
        return 
 
+#User model 
+class User(models.Model):
+    first_name = models.CharField(max_length=255)  
+    last_name = models.CharField(max_length=255)   
+    email = models.EmailField(unique=True)         
+    password = models.CharField(max_length=255)    
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+#SignUp model to track user registrations
+class SignUp(models.Model):
+    #links sign up to the user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
+    # storing the date signed-up 
+    date_signed_up = models.DateTimeField(auto_now_add=True)     
+
+    def __str__(self):
+        return f"SignUp for {self.user.email}"
+
+#SignIn model
+class SignIn(models.Model):
+    #user email as an id
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE) 
+    #store password
+    password = models.CharField(max_length=255)                  
+    #sign in time if desired
+    date_signed_in = models.DateTimeField(auto_now_add=True)     
+
+    def __str__(self):
+        return f"SignIn for {self.user_id.email} on {self.date_signed_in}
 
