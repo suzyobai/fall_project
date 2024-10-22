041b65626b9c9ede5407baa70273384017c7bfa6
unknown <spandanaa10@gmail.com>
2024-10-21 22:17:58 -0400

    removing the user db

--- test/myfirstproj/myfirstproj/models.py
+++ test/myfirstproj/myfirstproj/models.py
@@ -56,35 +56,3 @@ class UserProfile(models.Model):
        return self.user.username
 
 
-#User model 
-class User(models.Model):
-    first_name = models.CharField(max_length=255)  
-    last_name = models.CharField(max_length=255)   
-    email = models.EmailField(unique=True)         
-    password = models.CharField(max_length=255)    
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
-
-#SignUp model to track user registrations
-class SignUp(models.Model):
-    #links sign up to the user
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  
-    # storing the date signed-up 
-    date_signed_up = models.DateTimeField(auto_now_add=True)     
-
-    def __str__(self):
-        return f"SignUp for {self.user.email}"
-
-#SignIn model
-class SignIn(models.Model):
-    #user email as an id
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE) 
-    #store password
-    password = models.CharField(max_length=255)                  
-    #sign in time if desired
-    date_signed_in = models.DateTimeField(auto_now_add=True)     
-
-    def __str__(self):
-        return f"SignIn for {self.user_id.email} on {self.date_signed_in}"
-
