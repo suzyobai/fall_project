b620ba6f32cd405d96d2d353da8958364e9dae80
“Staxupgit config --global user.email “nuhshekhey@gmail.com <nuhshekhey@gmail.com>
2024-11-05 14:28:04 -0500

    added login and logout

--- test/home/urls.py
+++ test/home/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('', views.home, name='home'),  # Sets the home view as the homepage for ref.
     path('home/', views.home, name='home'),
     path('view_reviews/', views.view_reviews, name='view_reviews'),  # Pushes the data for reviews 
+    path('login/', views.LoginInterfaceView.as_view(), name='login'), #added for login
+    path('logout/', views.LogoutInterfaceView.as_view(), name='logout'), #added for logout
+
     # path('blog/', include("blog.urls")),  # Uncomment if adding blog app
 ]
 
